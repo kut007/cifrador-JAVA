fe69d11545307a4798085bf31f8daaa2bf1288b1
kut007 <antonio.rita.fer@gmail.com>
2026-04-14 08:43:34 -0300

    Adicionado os replaces

--- Main.java
+++ Main.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in)
-        System.out.print("Digite sua frase: ")
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite sua frase: ");
         String frase = ler.nextLine;
+
+        String removida = remover(frase);
+        System.out.println(removida);
     }
 
 
     static String remover(frase) {
-        frase.toUpperCase();
-        for (int i =0;i < frase.lenght(); i++) {
-
-        }
+        frase = frase.toUpperCase();
+        frase = frase.replace(" ", "");
+        frase = frase.replace(".", "");
+        frase = frase.replace(",", "");
+        frase = frase.replace(";", "");
+        frase = frase.replace("?", "");
+        frase = frase.replace(":", "");
+        frase = frase.replace("-", "");
+        frase = frase.replace("Á", "A");
+        frase = frase.replace("É", "E");
+        frase = frase.replace("Í", "I");
+        frase = frase.replace("Ó", "O");
+        frase = frase.replace("Ú", "U");
+        frase = frase.replace("Ç", "C");
+        frase = frase.replace("Ã", "A");    
+        return frase;    
     }
 }
