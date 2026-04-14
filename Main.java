d7f930f00cdbc459b7d741c5df192cf8f052fc84
kut007 <antonio.rita.fer@gmail.com>
2026-04-14 07:54:24 -0300

    Codigo iniciado

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in)
+        System.out.print("Digite sua frase: ")
+        String frase = ler.nextLine;
+    }
+
+
+    static String remover(frase) {
+        frase.toUpperCase();
+        for (int i =0;i < frase.lenght(); i++) {
+
+        }
+    }
+}
